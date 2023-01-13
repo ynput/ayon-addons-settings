3d9e49531cca5fac2d2855adfd3bb101bbb25b6d
Jakub Trllo <jakub.trllo@gmail.com>
2023-01-13 11:53:43 +0100

    use 'TemplateWorkfileBaseOptions' from base settins model

--- blender/dev/settings/main.py
+++ blender/dev/settings/main.py
@@ -1,40 +1,18 @@
 from ayon_server.settings import (
     Field,
     BaseSettingsModel,
-    PathModel
+    TemplateWorkfileBaseOptions
 )
-from ayon_server.settings.enum import task_types_enum
 
 from .publish_plugins import (
     PublishPuginsModel,
     DEFAULT_BLENDER_PUBLISH_SETTINGS
 )
 
-class CustomTemplateModel(BaseSettingsModel):
-    _layout = "expanded"
-    _isGroup = True
-    task_types: list[str] = Field(
-        default_factory=list,
-        title="Task types",
-        enum_resolver=task_types_enum
-    )
-    path: PathModel = Field(default_factory=PathModel, title="Path")
-
-
-class BlenderTemplateWorkfileOptions(BaseSettingsModel):
-    create_first_version: bool = Field(
-        False,
-        title="Create first workfile",
-    )
-    custom_templates: list[CustomTemplateModel] = Field(
-        default_factory=list,
-        title="Custom templates",
-    )
-
 
 class BlenderSettings(BaseSettingsModel):
-    workfile_builder: BlenderTemplateWorkfileOptions = Field(
-        default_factory=BlenderTemplateWorkfileOptions,
+    workfile_builder: TemplateWorkfileBaseOptions = Field(
+        default_factory=TemplateWorkfileBaseOptions,
         title="Workfile Builder"
     )
 
