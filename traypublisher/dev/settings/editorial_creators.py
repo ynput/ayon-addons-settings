7abf34813d4433fdf70330e2c59f8012d5806706
Jakub Trllo <jakub.trllo@gmail.com>
2023-01-13 15:39:56 +0100

    fix traypublisher settings model

--- traypublisher/dev/settings/editorial_creators.py
+++ traypublisher/dev/settings/editorial_creators.py
@@ -3,13 +3,6 @@ from pydantic import Field
 from ayon_server.settings import BaseSettingsModel
 
 
-class ClipNameTokenizerItem(BaseSettingsModel):
-    _layout = "expanded"
-    # TODO was 'dict-modifiable', is list of dicts now, must be fixed in code
-    name: str = Field('#TODO', title="Tokenizer name")
-    regex: str = Field('', title="Tokenizer regex")
-
-
 task_types_enum = [
     {"label": "Generic", "value": "Generic"},
     {"label": "Art", "value": "Art"},
@@ -30,21 +23,27 @@ task_types_enum = [
 ]
 
 
+class ClipNameTokenizerItem(BaseSettingsModel):
+    _layout = "expanded"
+    # TODO was 'dict-modifiable', is list of dicts now, must be fixed in code
+    name: str = Field("#TODO", title="Tokenizer name")
+    regex: str = Field("", title="Tokenizer regex")
+
+
 class ShotAddTasksItem(BaseSettingsModel):
     _layout = "expanded"
     # TODO was 'dict-modifiable', is list of dicts now, must be fixed in code
     name: str = Field('', title="Key")
     task_type: list[str] = Field(
-                                title="Task type",
-                                default_factory=list,
-                                enum_resolver=lambda: task_types_enum)
+        title="Task type",
+        default_factory=list,
+        enum_resolver=lambda: task_types_enum)
 
 
 class ShotRenameSubmodel(BaseSettingsModel):
     enabled: bool = True
-
     shot_rename_template: str = Field(
-        '',
+        "",
         title="Shot rename template"
     )
 
@@ -64,12 +63,12 @@ class TokenToParentConvertorItem(BaseSettingsModel):
         enum_resolver=lambda: parent_type_enum
     )
     name: str = Field(
-        '',
+        "",
         title="Parent token name",
         description="Unique name used in `Parent path template`"
     )
     value: str = Field(
-        '',
+        "",
         title="Parent token value",
         description="Template where any text, Anatomy keys and Tokens could be used"  # noqa
     )
@@ -77,13 +76,11 @@ class TokenToParentConvertorItem(BaseSettingsModel):
 
 class ShotHierchySubmodel(BaseSettingsModel):
     enabled: bool = True
-
     parents_path: str = Field(
-        '',
+        "",
         title="Parents path template",
         description="Using keys from \"Token to parent convertor\" or tokens directly"  # noqa
     )
-
     parents: list[TokenToParentConvertorItem] = Field(
         default_factory=TokenToParentConvertorItem,
         title="Token to parent convertor"
@@ -98,74 +95,43 @@ output_file_type = [
 
 
 class FamilyPresetItem(BaseSettingsModel):
-    family: str = Field(
-        '',
-        title="Family"
-    )
-
-    # TODO ? in v3 shows up grayed out  'inherited'??
-    variant: str = Field(
-        '',
-        title='Variant'
-    )
-
-    review: bool = Field(
-        False,
-        title="Review"
-    )
-
+    family: str = Field("", title="Family")
+    # TODO add placeholder '< Inherited >'
+    variant: str = Field("", title="Variant")
+    review: bool = Field(True, title="Review")
     output_file_type: str = Field(
         ".mp4",
         enum_resolver=lambda: output_file_type
     )
 
 
-class ShotSubsetCreator(BaseSettingsModel):
-    # TODO collapsible-wrap is not implemented, this one introduces additional
-    # level with key >> breaks defaults template >> template in main modified
-
-    family_presets: list[FamilyPresetItem] = Field(
-        default_factory=FamilyPresetItem
+class EditorialSimpleCreatorPlugin(BaseSettingsModel):
+    default_variants: list[str] = Field(
+        default_factory=list,
+        title="Default Variants"
     )
-
-
-class ShotMetadataCreator(BaseSettingsModel):
-    # TODO collapsible-wrap is not implemented, this one introduces additional
-    # level with key >> breaks defaults template >> template in main modified
-
     clip_name_tokenizer: list[ClipNameTokenizerItem] = Field(
         default_factory=ClipNameTokenizerItem,
-        description="Using Regex expression to create tokens. \nThose can be used later in \"Shot rename\" creator \nor \"Shot hierarchy\". \n\nTokens should be decorated with \"_\" on each side",  # noqa
+        description=(
+            "Using Regex expression to create tokens. \nThose can be used"
+            " later in \"Shot rename\" creator \nor \"Shot hierarchy\"."
+            "\n\nTokens should be decorated with \"_\" on each side"
+        )
     )
-
     shot_rename: ShotRenameSubmodel = Field(
         title="Shot Rename",
         default_factory=ShotRenameSubmodel
     )
-
     shot_hierarchy: ShotHierchySubmodel = Field(
         title="Shot Hierarchy",
         default_factory=ShotHierchySubmodel
     )
-
     shot_add_tasks: list[ShotAddTasksItem] = Field(
         title="Add tasks to shot",
         default_factory=ShotAddTasksItem
     )
-
-
-class EditorialSimpleCreatorPlugin(BaseSettingsModel):
-    default_variants: list[str] = Field(default_factory=list,
-                                        title="Default Variants")
-
-    shot_metadata_creator: ShotMetadataCreator = Field(
-        title="Shot metadata creator",
-        default_factory=ShotMetadataCreator,
-    )
-
-    shot_subset_creator: ShotSubsetCreator = Field(
-        title="Shot's subset creator",
-        default_factory=ShotSubsetCreator
+    family_presets: list[FamilyPresetItem] = Field(
+        default_factory=FamilyPresetItem
     )
 
 
@@ -174,3 +140,62 @@ class TraypublisherEditorialCreatorPlugins(BaseSettingsModel):
         title="Editorial simple creator",
         default_factory=EditorialSimpleCreatorPlugin,
     )
+
+
+DEFAULT_EDITORIAL_CREATORS= {
+    "editorial_simple": {
+        "default_variants": [
+            "Main"
+        ],
+        "clip_name_tokenizer": [
+            {"name": "_sequence_", "regex": "(sc\\d{3})"},
+            {"name": "_shot_", "regex": "(sh\\d{3})"}
+        ],
+        "shot_rename": {
+            "enabled": True,
+            "shot_rename_template": "{project[code]}_{_sequence_}_{_shot_}"
+        },
+        "shot_hierarchy": {
+            "enabled": True,
+            "parents_path": "{project}/{folder}/{sequence}",
+            "parents": [
+                {
+                    "parent_type": "Project",
+                    "name": "project",
+                    "value": "{project[name]}"
+                },
+                {
+                    "parent_type": "Folder",
+                    "name": "folder",
+                    "value": "shots"
+                },
+                {
+                    "parent_type": "Sequence",
+                    "name": "sequence",
+                    "value": "{_sequence_}"
+                }
+            ]
+        },
+        "shot_add_tasks": [],
+        "family_presets": [
+            {
+                "family": "review",
+                "variant": "Reference",
+                "review": True,
+                "output_file_type": ".mp4"
+            },
+            {
+                "family": "plate",
+                "variant": "",
+                "review": False,
+                "output_file_type": ".mov"
+            },
+            {
+                "family": "audio",
+                "variant": "",
+                "review": False,
+                "output_file_type": ".wav"
+            }
+        ]
+    }
+}
--- traypublisher/dev/settings/simple_creators.py
+++ traypublisher/dev/settings/simple_creators.py
@@ -5,40 +5,273 @@ from ayon_server.settings import BaseSettingsModel
 
 class SimpleCreatorPlugin(BaseSettingsModel):
     _layout = "expanded"
-    family: str = Field('',
-                        title="Family")
-
-    identifier: str = Field('',              # TODO add placeholder
-                            title="Identifier")
-
-    label: str = Field('',
-                       title="Label")
-
-    icon: str = Field('',
-                      title="Icon")
-
-    default_variants: list[str] = Field(default_factory=list,
-                                        title="Default Variants")
-
+    family: str = Field("", title="Family")
+    # TODO add placeholder
+    identifier: str = Field("", title="Identifier")
+    label: str = Field("", title="Label")
+    icon: str = Field("", title="Icon")
+    default_variants: list[str] = Field(
+        default_factory=list,
+        title="Default Variants"
+    )
     description: str = Field(
-        '',
+        "",
         title="Description",
         widget="textarea"
     )
-
     detailed_description: str = Field(
-        '',
+        "",
         title="Detailed Description",
         widget="textarea"
     )
-
+    allow_sequences: bool = Field(
+        False,
+        title="Allow sequences"
+    )
     allow_multiple_items: bool = Field(
         False,
         title="Allow multiple items"
     )
-
     extensions: list[str] = Field(
         default_factory=list,
         title="Extensions"
     )
 
+
+DEFAULT_SIMPLE_CREATORS = [
+    {
+        "family": "workfile",
+        "identifier": "",
+        "label": "Workfile",
+        "icon": "fa.file",
+        "default_variants": [
+            "Main"
+        ],
+        "description": "Backup of a working scene",
+        "detailed_description": "Workfiles are full scenes from any application that are directly edited by artists. They represent a state of work on a task at a given point and are usually not directly referenced into other scenes.",
+        "allow_sequences": False,
+        "allow_multiple_items": False,
+        "extensions": [
+            ".ma",
+            ".mb",
+            ".nk",
+            ".hrox",
+            ".hip",
+            ".hiplc",
+            ".hipnc",
+            ".blend",
+            ".scn",
+            ".tvpp",
+            ".comp",
+            ".zip",
+            ".prproj",
+            ".drp",
+            ".psd",
+            ".psb",
+            ".aep"
+        ]
+    },
+    {
+        "family": "model",
+        "identifier": "",
+        "label": "Model",
+        "icon": "fa.cubes",
+        "default_variants": [
+            "Main",
+            "Proxy",
+            "Sculpt"
+        ],
+        "description": "Clean models",
+        "detailed_description": "Models should only contain geometry data, without any extras like cameras, locators or bones.\n\nKeep in mind that models published from tray publisher are not validated for correctness. ",
+        "allow_sequences": False,
+        "allow_multiple_items": True,
+        "extensions": [
+            ".ma",
+            ".mb",
+            ".obj",
+            ".abc",
+            ".fbx",
+            ".bgeo",
+            ".bgeogz",
+            ".bgeosc",
+            ".usd",
+            ".blend"
+        ]
+    },
+    {
+        "family": "pointcache",
+        "identifier": "",
+        "label": "Pointcache",
+        "icon": "fa.gears",
+        "default_variants": [
+            "Main"
+        ],
+        "description": "Geometry Caches",
+        "detailed_description": "Alembic or bgeo cache of animated data",
+        "allow_sequences": True,
+        "allow_multiple_items": True,
+        "extensions": [
+            ".abc",
+            ".bgeo",
+            ".bgeogz",
+            ".bgeosc"
+        ]
+    },
+    {
+        "family": "plate",
+        "identifier": "",
+        "label": "Plate",
+        "icon": "mdi.camera-image",
+        "default_variants": [
+            "Main",
+            "BG",
+            "Animatic",
+            "Reference",
+            "Offline"
+        ],
+        "description": "Footage Plates",
+        "detailed_description": "Any type of image seqeuence coming from outside of the studio. Usually camera footage, but could also be animatics used for reference.",
+        "allow_sequences": True,
+        "allow_multiple_items": True,
+        "extensions": [
+            ".exr",
+            ".png",
+            ".dpx",
+            ".jpg",
+            ".tiff",
+            ".tif",
+            ".mov",
+            ".mp4",
+            ".avi"
+        ]
+    },
+    {
+        "family": "render",
+        "identifier": "",
+        "label": "Render",
+        "icon": "mdi.folder-multiple-image",
+        "default_variants": [],
+        "description": "Rendered images or video",
+        "detailed_description": "Sequence or single file renders",
+        "allow_sequences": True,
+        "allow_multiple_items": True,
+        "extensions": [
+            ".exr",
+            ".png",
+            ".dpx",
+            ".jpg",
+            ".jpeg",
+            ".tiff",
+            ".tif",
+            ".mov",
+            ".mp4",
+            ".avi"
+        ]
+    },
+    {
+        "family": "camera",
+        "identifier": "",
+        "label": "Camera",
+        "icon": "fa.video-camera",
+        "default_variants": [],
+        "description": "3d Camera",
+        "detailed_description": "Ideally this should be only camera itself with baked animation, however, it can technically also include helper geometry.",
+        "allow_sequences": False,
+        "allow_multiple_items": True,
+        "extensions": [
+            ".abc",
+            ".ma",
+            ".hip",
+            ".blend",
+            ".fbx",
+            ".usd"
+        ]
+    },
+    {
+        "family": "image",
+        "identifier": "",
+        "label": "Image",
+        "icon": "fa.image",
+        "default_variants": [
+            "Reference",
+            "Texture",
+            "Concept",
+            "Background"
+        ],
+        "description": "Single image",
+        "detailed_description": "Any image data can be published as image family. References, textures, concept art, matte paints. This is a fallback 2d family for everything that doesn't fit more specific family.",
+        "allow_sequences": False,
+        "allow_multiple_items": True,
+        "extensions": [
+            ".exr",
+            ".jpg",
+            ".jpeg",
+            ".dpx",
+            ".bmp",
+            ".tif",
+            ".tiff",
+            ".png",
+            ".psb",
+            ".psd"
+        ]
+    },
+    {
+        "family": "vdb",
+        "identifier": "",
+        "label": "VDB Volumes",
+        "icon": "fa.cloud",
+        "default_variants": [],
+        "description": "Sparse volumetric data",
+        "detailed_description": "Hierarchical data structure for the efficient storage and manipulation of sparse volumetric data discretized on three-dimensional grids",
+        "allow_sequences": True,
+        "allow_multiple_items": True,
+        "extensions": [
+            ".vdb"
+        ]
+    },
+    {
+        "family": "matchmove",
+        "identifier": "",
+        "label": "Matchmove",
+        "icon": "fa.empire",
+        "default_variants": [
+            "Camera",
+            "Object",
+            "Mocap"
+        ],
+        "description": "Matchmoving script",
+        "detailed_description": "Script exported from matchmoving application to be later processed into a tracked camera with additional data",
+        "allow_sequences": False,
+        "allow_multiple_items": True,
+        "extensions": []
+    },
+    {
+        "family": "rig",
+        "identifier": "",
+        "label": "Rig",
+        "icon": "fa.wheelchair",
+        "default_variants": [],
+        "description": "CG rig file",
+        "detailed_description": "CG rigged character or prop. Rig should be clean of any extra data and directly loadable into it's respective application\t",
+        "allow_sequences": False,
+        "allow_multiple_items": False,
+        "extensions": [
+            ".ma",
+            ".blend",
+            ".hip",
+            ".hda"
+        ]
+    },
+    {
+        "family": "simpleUnrealTexture",
+        "identifier": "",
+        "label": "Simple UE texture",
+        "icon": "fa.image",
+        "default_variants": [],
+        "description": "Simple Unreal Engine texture",
+        "detailed_description": "Texture files with Unreal Engine naming conventions",
+        "allow_sequences": False,
+        "allow_multiple_items": True,
+        "extensions": []
+    }
+]
