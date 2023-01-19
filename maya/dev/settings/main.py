76eba22a7eb47225f2253be9f3e24ebf81e8bd1b
Jakub Trllo <jakub.trllo@gmail.com>
2023-01-19 17:51:47 +0100

    match v3 settings structure

--- maya/dev/settings/main.py
+++ maya/dev/settings/main.py
@@ -1,8 +1,6 @@
 from pydantic import Field
 from ayon_server.settings.common import BaseSettingsModel
 from .imageio import ImageIOSettings, DEFAULT_IMAGEIO_SETTINGS
-from .mel_workspace import MelWorkspaceModel, DEFAULT_MEL_WORKSPACE_SETTINGS
-from .ext_mapping import ExtMappingModel, DEFAULT_EXT_MAPPING_SETTINGS
 from .maya_dirmap import MayaDirmapModel, DEFAULT_MAYA_DIRMAP_SETTINGS
 from .scriptsmenu import ScriptsmenuModel, DEFAULT_SCRIPTSMENU_SETTINGS
 from .render_settings import RenderSettingsModel, DEFAULT_RENDER_SETTINGS
@@ -13,28 +11,34 @@ from .workfile_build_settings import ProfilesModel, DEFAULT_WORKFILE_SETTING
 from .templated_workfile_settings import TemplatedProfilesModel, DEFAULT_TEMPLATED_WORKFILE_SETTINGS
 from .publish_gui_filters import PublishFiltersModel, DEFAULT_GUI_FILTERS_SETTINGS
 
+
+class ExtMappingItemModel(BaseSettingsModel):
+    _layout = "compact"
+    name: str = Field(title="Family")
+    value: str = Field(title="Extension")
+
+
 class MayaSettings(BaseSettingsModel):
     """Maya Project Settings."""
     _layout = "expanded"
 
     imageio: ImageIOSettings = Field(
         default_factory=ImageIOSettings, title="Color Management (imageio)")
-    mel_workspace: MelWorkspaceModel = Field(
-        default_factory=MelWorkspaceModel, title="Maya MEL Workspace")
-    ext_mapping: ExtMappingModel = Field(
-        default_factory=ExtMappingModel, title="Extension Mapping")
+    mel_workspace: str = Field(title="Maya MEL Workspace", widget="textarea")
+    ext_mapping: list[ExtMappingItemModel] = Field(
+        default_factory=list, title="Extension Mapping")
     maya_dirmap: MayaDirmapModel = Field(
         default_factory=MayaDirmapModel, title="Maya dirmap Settings")
     scriptsmenu: ScriptsmenuModel = Field(
         default_factory=ScriptsmenuModel, title="Scriptsmenu Settings")
     render_settings: RenderSettingsModel = Field(
         default_factory=RenderSettingsModel, title="Render Settings")
-    creators: CreatorsModel = Field(
+    create: CreatorsModel = Field(
         default_factory=CreatorsModel, title="Creators")
-    publishers: PublishersModel= Field(
+    publish: PublishersModel= Field(
         default_factory=PublishersModel, title="Publishers"
     )
-    loaders: LoadersModel = Field(
+    load: LoadersModel = Field(
         default_factory=LoadersModel, title="Loaders"
     )
     workfile_build: ProfilesModel = Field(
@@ -48,17 +52,37 @@ class MayaSettings(BaseSettingsModel):
         default_factory=PublishFiltersModel, title="Publish GUI Filters")
 
 
+DEFAULT_MEL_WORKSPACE_SETTINGS = "\n".join((
+    'workspace -fr "shaders" "renderData/shaders";',
+    'workspace -fr "images" "renders/maya";',
+    'workspace -fr "particles" "particles";',
+    'workspace -fr "mayaAscii" "";',
+    'workspace -fr "mayaBinary" "";',
+    'workspace -fr "scene" "";',
+    'workspace -fr "alembicCache" "cache/alembic";',
+    'workspace -fr "renderData" "renderData";',
+    'workspace -fr "sourceImages" "sourceimages";',
+    'workspace -fr "fileCache" "cache/nCache";',
+))
+
 DEFAULT_MAYA_SETTING = {
     "imageio": DEFAULT_IMAGEIO_SETTINGS,
     "mel_workspace": DEFAULT_MEL_WORKSPACE_SETTINGS,
-    "ext_mapping": DEFAULT_EXT_MAPPING_SETTINGS,
+    "ext_mapping": [
+        {"name": "model", "value": "ma"},
+        {"name": "mayaAscii", "value": "ma"},
+        {"name": "camera", "value": "ma"},
+        {"name": "rig", "value": "ma"},
+        {"name": "workfile", "value": "ma"},
+        {"name": "yetiRig", "value": "ma"}
+    ],
     # `maya_dirmap` was originally with dash - `maya-dirmap`
     "maya_dirmap": DEFAULT_MAYA_DIRMAP_SETTINGS,
     "scriptsmenu": DEFAULT_SCRIPTSMENU_SETTINGS,
     "render_settings": DEFAULT_RENDER_SETTINGS,
-    "creators": DEFAULT_CREATORS_SETTINGS,
-    "publishers": DEFAULT_PUBLISH_SETTINGS,
-    "loaders": DEFAULT_LOADERS_SETTING,
+    "create": DEFAULT_CREATORS_SETTINGS,
+    "publish": DEFAULT_PUBLISH_SETTINGS,
+    "load": DEFAULT_LOADERS_SETTING,
     "workfile_build": DEFAULT_WORKFILE_SETTING,
     "templated_workfile_build": DEFAULT_TEMPLATED_WORKFILE_SETTINGS,
     "filters": DEFAULT_GUI_FILTERS_SETTINGS
