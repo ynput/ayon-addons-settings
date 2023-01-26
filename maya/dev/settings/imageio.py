0efb479177faf6673f1d147bb65b3693db5fc426
Jakub Trllo <jakub.trllo@gmail.com>
2023-01-26 11:23:50 +0100

    fix keys in maya imageio

--- maya/dev/settings/imageio.py
+++ maya/dev/settings/imageio.py
@@ -4,36 +4,31 @@ Note: Names were changed to get rid of the versions in class names.
 """
 from pydantic import Field
 
-from ayon_server.settings import BaseSettingsModel
+from ayon_server.settings import BaseSettingsModel, MultiplatformPathListModel
 
 
-class PathsTemplate(BaseSettingsModel):
-    """Path template for multiplatform paths.
-
-    Todo:
-        This should be unified across the addons.
-    """
-    windows: list[str] = Field(title="Windows")
-    darwin: list[str] = Field(title="MacOS")
-    linux: list[str] = Field(title="Linux")
-
-
-class ColorManagementPreferencesModel(BaseSettingsModel):
+class ColorManagementPreferenceV2Model(BaseSettingsModel):
     """Color Management Preference v2 (Maya 2022+)."""
     _layout = "expanded"
 
     enabled: bool = Field(True, title="Use Color Management Preference v2")
-    configFilePath: PathsTemplate = Field(default_factory=PathsTemplate, title="OCIO Config File Path")
+    configFilePath: MultiplatformPathListModel = Field(
+        default_factory=MultiplatformPathListModel,
+        title="OCIO Config File Path"
+    )
     renderSpace: str = Field(title="Rendering Space")
     displayName: str = Field(title="Display")
     viewName: str = Field(title="View")
 
 
-class ColorManagementPreferencesLegacyModel(BaseSettingsModel):
+class ColorManagementPreferenceModel(BaseSettingsModel):
     """Color Management Preference (legacy)."""
     _layout = "expanded"
 
-    configFilePath: PathsTemplate = Field(default_factory=PathsTemplate, title="OCIO Config File Path")
+    configFilePath: MultiplatformPathListModel = Field(
+        default_factory=MultiplatformPathListModel,
+        title="OCIO Config File Path"
+    )
     renderSpace: str = Field(title="Rendering Space")
     viewTransform: str = Field(title="Viewer Transform ")
 
@@ -46,33 +41,33 @@ class ImageIOSettings(BaseSettingsModel):
 
     _isGroup: bool = True
 
-    ColorManagementPreferences: ColorManagementPreferencesModel = Field(
-        default_factory=ColorManagementPreferencesModel,
+    colorManagementPreference_v2: ColorManagementPreferenceV2Model = Field(
+        default_factory=ColorManagementPreferenceV2Model,
         title="Color Management Preference v2 (Maya 2022+)"
     )
-    ColorManagementPreferencesLegacy: ColorManagementPreferencesLegacyModel = Field(
-        default_factory=ColorManagementPreferencesLegacyModel,
+    colorManagementPreference: ColorManagementPreferenceModel = Field(
+        default_factory=ColorManagementPreferenceModel,
         title="Color Management Preference (legacy)"
     )
 
 
 DEFAULT_IMAGEIO_SETTINGS = {
-        "ColorManagementPreferences": {
+        "colorManagementPreference_v2": {
             "enabled": True,
             "configFilePath": {
-                "windows": [""],
-                "darwin": [""],
-                "linux": [""]
+                "windows": [],
+                "darwin": [],
+                "linux": []
             },
             "renderSpace": "ACEScg",
             "displayName": "sRGB",
             "viewName": "ACES 1.0 SDR-video"
         },
-        "ColorManagementPreferencesLegacy": {
+        "colorManagementPreference": {
             "configFilePath": {
-                "windows": [""],
-                "darwin": [""],
-                "linux": [""]
+                "windows": [],
+                "darwin": [],
+                "linux": []
             },
             "renderSpace": "scene-linear Rec 709/sRGB",
             "viewTransform": "sRGB gamma"
