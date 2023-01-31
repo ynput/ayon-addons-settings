9defcbe3a149a5a653e841f1c8d17194a64c62bf
Jakub Trllo <jakub.trllo@gmail.com>
2023-01-31 12:24:16 +0100

    change filters

--- tvpaint/dev/settings/main.py
+++ tvpaint/dev/settings/main.py
@@ -1,5 +1,8 @@
 from pydantic import Field, validator
-from ayon_server.settings import BaseSettingsModel, Field
+from ayon_server.settings import (
+    BaseSettingsModel,
+    ensure_unique_names,
+)
 
 from .workfile_builder import WorkfileBuilderPlugin
 
@@ -8,14 +11,22 @@ from .plugins import (
     LoadPluginsModel,
 )
 
-class PublishFiltersModel(BaseSettingsModel):
-    _layout = "expanded"
+
+class PublishGUIFilterItemModel(BaseSettingsModel):
+    _layout = "compact"
     name: str = Field(title="Name")
-    value: str = Field(
-        "",
-        title="JSON",
-        widget="textarea",
-    )
+    value: bool = Field(True, title="Active")
+
+
+class PublishGUIFiltersModel(BaseSettingsModel):
+    _layout = "compact"
+    name: str = Field(title="Name")
+    value: list[PublishGUIFilterItemModel] = Field(default_factory=list)
+
+    @validator("value")
+    def validate_unique_outputs(cls, value):
+        ensure_unique_names(value)
+        return value
 
 
 class TvpaintSettings(BaseSettingsModel):
@@ -29,9 +40,14 @@ class TvpaintSettings(BaseSettingsModel):
         default_factory=WorkfileBuilderPlugin,
         title="Workfile Builder"
     )
-    filters: list[PublishFiltersModel] = Field(
-        default_factory=list
-    )
+    filters: list[PublishGUIFiltersModel] = Field(
+        default_factory=list,
+        title="Publish GUI Filters")
+
+    @validator("filters")
+    def validate_unique_outputs(cls, value):
+        ensure_unique_names(value)
+        return value
 
 
 DEFAULT_VALUES = {
@@ -95,5 +111,5 @@ DEFAULT_VALUES = {
         "create_first_version": False,
         "custom_templates": []
     },
-    # "filters": {}
+    "filters": []
 }
