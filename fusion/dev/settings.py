70cd66c2e56cb9a42391b12335cfaef94a905060
Jakub Trllo <jakub.trllo@gmail.com>
2023-04-12 12:23:38 +0200

    updated fusion settings

--- fusion/dev/settings.py
+++ fusion/dev/settings.py
@@ -3,14 +3,14 @@ from ayon_server.settings import (
     BaseSettingsModel,
     ImageIOConfigModel,
     ImageIOFileRulesModel,
-    MultiplatformPathModel,
+    MultiplatformPathListModel,
 )
 
 
 class OcioModel(BaseSettingsModel):
     enabled: bool = False
-    configFilePath: MultiplatformPathModel = Field(
-        default_factory=MultiplatformPathModel,
+    configFilePath: MultiplatformPathListModel = Field(
+        default_factory=MultiplatformPathListModel,
         title="OCIO Config File Path"
     )
 
@@ -30,8 +30,45 @@ class imageioModel(BaseSettingsModel):
     )
 
 
+class CopyFusionSettingsModel(BaseSettingsModel):
+    copy_path: str = Field("", title="Local Fusion profile directory")
+    copy_status: bool = Field(title="Copy profile on first launch")
+    force_sync: bool = Field(title="Resync profile on each launch")
+
+
 class FusionSettings(BaseSettingsModel):
     imageio: imageioModel = Field(
         default_factory=imageioModel,
         title="Color Management (ImageIO)"
     )
+    copy_fusion_settings: CopyFusionSettingsModel = Field(
+        default_factory=CopyFusionSettingsModel,
+        title="Local Fusion profile settings"
+    )
+
+
+DEFAULT_VALUES = {
+    "imageio": {
+        "ocio_config": {
+            "enabled": False,
+            "filepath": []
+        },
+        "file_rules": {
+            "enabled": False,
+            "rules": []
+        },
+        "ocio": {
+            "enabled": False,
+            "configFilePath": {
+                "windows": [],
+                "darwin": [],
+                "linux": []
+            }
+        }
+    },
+    "copy_fusion_settings": {
+        "copy_path": "~/.openpype/hosts/fusion/profiles",
+        "copy_status": False,
+        "force_sync": False
+    }
+}
